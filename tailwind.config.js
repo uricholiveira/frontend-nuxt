8f2551081a333e6b9b15d5d3774d479e15a47878
Urich Oliveira <oliveira.urich@gmail.com>
2023-04-19 23:12:50 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./node_modules/flowbite.{js,ts}"
+  ],
+  plugins: [
+    require('flowbite')
+  ],
+  // make sure to safelist these classes when using purge
+  safelist: [
+    'w-64',
+    'w-1/2',
+    'rounded-l-lg',
+    'rounded-r-lg',
+    'bg-gray-200',
+    'grid-cols-4',
+    'grid-cols-7',
+    'h-6',
+    'leading-6',
+    'h-9',
+    'leading-9',
+    'shadow-lg'
+  ],
+
+  // enable dark mode via class strategy
+  darkMode: 'class',
+
+  theme: {
+    extend: {
+      // extend base Tailwind CSS utility classes
+    }
+  },
+}
